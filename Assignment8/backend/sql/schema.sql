781ae900f4d28d54fe4ff1a8f972bcc2cf16dcfb
mrAstro22 <jaaalons@ucsc.edu>
2026-05-04 20:05:12 -0700

    pray

--- Assignment8/backend/sql/schema.sql
+++ Assignment8/backend/sql/schema.sql
@@ -1,37 +1,35 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+ 
 DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS groups CASCADE;
 DROP TABLE IF EXISTS grouproles CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS post_likes CASCADE;
-
+ 
 CREATE TABLE users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   data JSONB
 );
-
--- Group ID and Group Metadata(groupname, description)
+ 
 CREATE TABLE groups (
-  -- IF user is deleted, so will all their posts
-  groupid UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
+  groupid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   data jsonb
 );
-
--- Group ID, User ID,  and Role
+ 
 CREATE TABLE grouproles (
   groupid UUID NOT NULL REFERENCES groups(groupid) ON DELETE CASCADE,
   userid UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   data jsonb,
   PRIMARY KEY (groupid, userid)
 );
-
+ 
 CREATE TABLE posts (
-  -- IF user is deleted, so will all their posts
-  postid UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
+  postid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   userid UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   groupid UUID REFERENCES groups(groupid) ON DELETE CASCADE,
   data jsonb
 );
-
+ 
 CREATE TABLE post_likes (
   postid UUID NOT NULL REFERENCES posts(postid) ON DELETE CASCADE,
   userid UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
