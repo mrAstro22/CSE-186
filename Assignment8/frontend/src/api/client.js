22ea957e052ee624491d553f939aaf6fb8d57f9e
mrAstro22 <jaaalons@ucsc.edu>
2026-05-05 17:17:48 -0700

    slashFrontend

--- Assignment8/frontend/src/api/client.js
+++ Assignment8/frontend/src/api/client.js
@@ -1,21 +1,13 @@
-// This API helper is to connect the frontend and backend on render
-// Utilizing Environent Variables defined on Render
-// Removes need for fetch(localhost)
-
-/* global fetch */
-/* eslint-env browser */
 const API = import.meta.env.VITE_API_URL;
 
-/**
- *
- * @param path
- * @param options
- */
 export function apiFetch(path, options = {}) {
-  return fetch(`${API}${path}`, {
+  const base = API.replace(/\/$/, ''); // remove trailing slash safely
+
+  return fetch(`${base}${path}`, {
     ...options,
     headers: {
       Accept: 'application/json',
+      'Content-Type': 'application/json',
       ...(options.headers || {}),
     },
   });
