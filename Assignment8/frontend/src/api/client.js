17b4300476bd2ea9de5d6d5d5bac1945cf352731
mrAstro22 <jaaalons@ucsc.edu>
2026-05-05 17:20:04 -0700

    frontendDeploy

--- Assignment8/frontend/src/api/client.js
+++ Assignment8/frontend/src/api/client.js
@@ -1,12 +1,17 @@
 const API = import.meta.env.VITE_API_URL;
 
+/**
+ *
+ * @param path
+ * @param options
+ */
 export function apiFetch(path, options = {}) {
   const base = API.replace(/\/$/, ''); // remove trailing slash safely
 
   return fetch(`${base}${path}`, {
     ...options,
     headers: {
-      Accept: 'application/json',
+      'Accept': 'application/json',
       'Content-Type': 'application/json',
       ...(options.headers || {}),
     },
