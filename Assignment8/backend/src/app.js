7d906ef46c1abb2e2fa4284a487d1f6a7db9028f
mrAstro22 <jaaalons@ucsc.edu>
2026-05-13 00:40:49 -0700

    bugScout

--- Assignment8/backend/src/app.js
+++ Assignment8/backend/src/app.js
@@ -35,6 +35,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
 const apiSpec = path.join(__dirname, '../api/openapi.yaml');
 
 const apidoc = yaml.load(fs.readFileSync(apiSpec, 'utf8'));
